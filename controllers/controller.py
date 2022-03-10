f6ef323a3c50c7e35e8b39d0e152e1fee9f065fe
Jack <jackowendickson@gmail.com>
2022-03-10 13:41:33 +0000

    adding functionality to the form, need to do an add_new_event funciton

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,7 +1,22 @@
-from flask import render_template
+from crypt import methods
+from tkinter.messagebox import YES
+from flask import render_template, request
 from app import app
 from models.event_calendar import event_list
+from models.event import Event
 
 @app.route('/home')
 def index():
+    return render_template('index.html', title="home", events=event_list)
+
+@app.route('/home', methods=['POST'])
+def add_event():
+    event_name = request.form["event-name"]
+    event_date = request.form["event-date"]
+    event_description = request.form["description"]
+    number_of_guests = 30
+    event_location = "yes"
+    new_event = Event(event_date, event_name, number_of_guests, event_location, event_description)
+    add_new_event(new_event)
+
     return render_template('index.html', title="home", events=event_list)
